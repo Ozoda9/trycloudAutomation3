f8250b9aa3679aed58c786d9b40e82ce732e34ee
enas <enaselsaffar81@gmail.com>
2022-11-14 20:31:05 -0600

    creat page for Tc04

---
+++ src/test/java/com/trycloud/pages/Tc04Page.java
@@ -0,0 +1,34 @@
+package com.trycloud.pages;
+
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class Tc04Page {
+
+    public FileModelPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath ="//label[@for='select_all_files']")
+    public WebElement checkBox;
+    @FindBy(xpath ="//input[@class='selectCheckBox checkbox'][1]")
+    public WebElement allCheckBox;
+
+
+    @FindBy(xpath = "//ul[@id='appmenu']//a")
+    public  static List<WebElement> allModules;
+
+
+    public static void clickEachModule(String  givenModule) {
+        for (WebElement each : allModules) {
+            if (each.getAttribute("aria-label").equals(givenModule)) {
+                each.click();
+                break;
+            }
+        }
+    }
+}
